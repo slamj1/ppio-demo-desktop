73827893801e7f15aee023401fd6389449dcc50f
ChangMM <1769111903@qq.com>
2018-10-30 17:34:13 +0800

    [fix] _var.scss & _variables.scss

--- src/views/account/Create.vue
+++ src/views/account/Create.vue
@@ -35,7 +35,7 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-@import '@/assets/css/_variables.scss';
+@import '@/assets/css/_var.scss';
 #account-create {
   padding: 100px 80px 0;
   text-align: center;
